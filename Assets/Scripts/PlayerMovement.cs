eddf7e927cdde16b5b1ad4a5d90cc47bd21e0ffc
lucas <umbreonbeyond@gmail.com>
2022-07-03 08:48:14 -0500

    fixed player controls

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,49 +6,41 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
-    private ControlSceme _control;
     public float speed;
     public float jumpHeight = 2;
-    private float gravityMuliplayer = 1f;
-
+    public Transform cameraRoot;
     private Rigidbody _rb;
-
+    private Vector3 moveForce;
+    private float xRotation = 0f;
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
-        _control = new ControlSceme();
-        _control.Player.Jump.performed += Jump_performed;
     }
-
-    private void Jump_performed(InputAction.CallbackContext obj)
-    {
-        float jumpforce = Mathf.Sqrt(jumpHeight * 2 * Physics.gravity.y);
-        _rb.AddForce(jumpforce * transform.up);
-
-    }
-
-    private void OnEnable()
-    {
-        _control.Player.Enable();
-    }
-    private void OnDisable()
-    {
-        _control.Player.Disable();
-    }
-
     private void FixedUpdate()
     {
-        Vector2 moveVal = _control.Player.Move.ReadValue<Vector2>();
-        Vector3 move = transform.forward * moveVal.y + transform.right * moveVal.x;
-        move *= speed;
-
-        _rb.AddForce(move * Time.deltaTime);
+        _rb.AddForce(moveForce,ForceMode.Force);
     }
-    public void ToggleGravity()
+    public void MovePlayer(InputAction.CallbackContext ctx)
     {
-        if (gravityMuliplayer == 0)
-            gravityMuliplayer = 1;
-        else
-            gravityMuliplayer = 0;
+        Vector2 input = ctx.ReadValue<Vector2>();
+        Vector3 move = transform.forward * input.y + transform.right * input.x;
+        moveForce = move * speed;
+    }
+    public void LookInput(InputAction.CallbackContext ctx)
+    {
+        Vector2 input = ctx.ReadValue<Vector2>();
+        transform.Rotate(transform.up, input.x * Time.deltaTime);
+        xRotation -= input.y * Time.deltaTime;
+        xRotation = Mathf.Clamp(xRotation,-90,90);
+        cameraRoot.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+
+    public void Jump(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed)
+        {
+            _rb.velocity = new Vector3(_rb.velocity.x, Mathf.Sqrt(-2 * Physics.gravity.y * jumpHeight), _rb.velocity.z);
+        }
     }
 }
+
