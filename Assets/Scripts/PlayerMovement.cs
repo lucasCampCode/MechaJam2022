0544522a1cb56a3212ad04522423bf857f05647d
lucas <umbreonbeyond@gmail.com>
2022-07-07 13:20:40 -0500

    updated player prefab and player controller

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,57 +3,81 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Camera")]
+    public Animator playerCam;
     public Transform cameraRoot;
-    public PlayerSettingsSO settings;
-    private Rigidbody _rb;
+    [Header("Physics gravity")]
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.4f;
+    public float _gravity = -9.81f;
+    public float jumpHeight = 2f;
+    [Header("Player Physics")]
+    public float cameraSensitivity = 0f;
+    public float playerSpeed = 0f;
+
+    private CharacterController _controller;
     private float xRotation = 0f;
     private Vector2 moveInput;
-
-    //----ARTIST CODE----
-    public Animator playerCam;
-    //----ARTIST CODE----
+    private Vector3 _velocity;
+    private bool isGrounded;
 
     void Start()
     {
-        _rb = GetComponent<Rigidbody>();
+        _controller = GetComponent<CharacterController>();
     }
     private void FixedUpdate()
     {
+        isGrounded = IsGrounded(Physics.OverlapSphere(groundCheck.position, groundCheckRadius));
+        //reset gravity velocity
+        if (isGrounded && _velocity.y < 0)
+            _velocity.y = -2f;
+        //move player based on player input
         Vector3 move = transform.forward * moveInput.y + transform.right * moveInput.x;
-        move *= settings.playerSpeed;
-        _rb.AddForce(move,ForceMode.Force);
-        
-        //----ARTIST CODE----
-        if (move.magnitude > 0f)
-        {
-            playerCam.SetBool("IsWalking", true);
-        }
-        else { playerCam.SetBool("IsWalking", false); }
-        //----ARTIST CODE----
-
+        move *= playerSpeed;
+        _controller.Move(move * Time.deltaTime);
+        //set animation walking perameter, based on if the player is moving
+        playerCam.SetBool("IsWalking", move.magnitude > 0f);
+        //apply gravity
+        _velocity.y += _gravity * Time.deltaTime;
+        _controller.Move(_velocity * Time.deltaTime);
     }
     public void MovePlayer(InputAction.CallbackContext ctx)
     {
+        //grab player input
         moveInput = ctx.ReadValue<Vector2>();
     }
     public void LookInput(InputAction.CallbackContext ctx)
     {
+        //grab player input
         Vector2 input = ctx.ReadValue<Vector2>();
-        transform.Rotate(transform.up, input.x * settings.cameraSensitivity * Time.deltaTime);
-        xRotation -= input.y * settings.cameraSensitivity * Time.deltaTime;
+        //rotate character on it y axis when player moves its mouse on the x axis
+        transform.Rotate(transform.up, input.x * cameraSensitivity * Time.deltaTime);
+        //rotate camera on it x axis when player moves its mouse on the y axis
+        xRotation -= input.y * cameraSensitivity * Time.deltaTime;
+        //clamp the rotation
         xRotation = Mathf.Clamp(xRotation,-90,90);
+        //set the new rotation for the camera
         cameraRoot.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
     }
-
     public void Jump(InputAction.CallbackContext ctx)
     {
-        if (ctx.performed)
+        if (ctx.performed && isGrounded)
         {
-            _rb.velocity = new Vector3(_rb.velocity.x, Mathf.Sqrt(-2 * Physics.gravity.y * settings.jumpHeight), _rb.velocity.z);
+            //change gravity velocity when the player jumps
+            _velocity.y = Mathf.Sqrt(-2 * _gravity * jumpHeight);
         }
+    }
+    private bool IsGrounded(Collider[] colliders)
+    {
+        foreach (Collider col in colliders)
+        {
+            if (col.gameObject != gameObject)//if any thing get hit that is not the player itself then return true
+                return true;
+        }
+        return false;
     }
 }
 
